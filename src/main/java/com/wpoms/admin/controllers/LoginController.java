b067fc6df36c1d114bc6d4f3b85e779156a08baa
Reshma <reshmarethu2504@gmail.com>
2026-04-20 10:58:13 +0530

    Intial commit - wpoms server

---
+++ src/main/java/com/wpoms/admin/controllers/LoginController.java
@@ -0,0 +1,24 @@
+package com.wpoms.admin.controllers;
+
+import com.wpoms.admin.models.payloads.LoginPayload;
+import com.wpoms.admin.models.response.LoginResponse;
+import com.wpoms.admin.services.ILoginService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin
+public class LoginController {
+
+    @Autowired
+    ILoginService service;
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@Valid @RequestBody LoginPayload payload) {
+        LoginResponse response = service.login(payload);
+        return ResponseEntity.ok(response);
+    }
+}
---
+++ src/main/java/com/wpoms/admin/services/IVendorService.java
@@ -0,0 +1,15 @@
+package com.wpoms.admin.services;
+
+import com.wpoms.admin.models.payloads.EditVendorPayload;
+import com.wpoms.admin.models.payloads.RegisterVendorPayload;
+import com.wpoms.admin.models.response.EditVendorResponse;
+import com.wpoms.admin.models.response.RegisterVendorResponse;
+
+public interface IVendorService {
+    RegisterVendorResponse registerVendor(RegisterVendorPayload payload);
+
+    RegisterVendorResponse getVendor(Integer id);
+
+    EditVendorResponse editVendor(Integer id, EditVendorPayload payload);
+
+}
